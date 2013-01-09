4afa5844cbbb642f11c05188055cb73d79bd1b10
dixkom <dixkom@hotmail.com>
2013-01-09 08:48:43 +0100

    added google analytics

--- webcolorsdetector.js
+++ webcolorsdetector.js
@@ -34,7 +34,7 @@ var wcd_loader = new function(){
 			this.loadjQueryUI();
 		}
 	};
-	
+
 	this.showFeedback = function(text){
 		if(document.getElementById('wcd_loader') != null){
 			document.getElementById('wcd_loader').innerHTML = text;
@@ -255,4 +255,14 @@ var wcd = new function() {
 
   
  wcd.init();
+ 
+   var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37491054-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
   
